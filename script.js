fe31f5115398d36fda0966bb695e2cda15851be4
davittrdzv <davittrdzv@gmail.com>
2024-10-17 12:23:49 -0600

    Commit 1. Archivo HTML y JS. Código de JS para evaluar distintas calificaciones, con un mensaje distinto según el rango de la calificación.

---
+++ script.js
@@ -0,0 +1,152 @@
+console.log("Los siguientes ejemplos muestran calificaciones con variables con un número ya asignado")
+let calificacion = 0
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 59
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 60
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 61
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 73
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 74
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 75
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 76
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 88
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 89
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 90
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 91
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 99
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+calificacion = 100
+if (calificacion >=90) {
+    console.log("Obtuviste", calificacion, ". ¡Aprobaste con una calificación excelente!");
+} else if (calificacion >=75 && calificacion <=89) {
+    console.log("Obtuviste", calificacion,". ¡Aprobaste con una calificación buena!");
+} else if (calificacion >=60 && calificacion <=74) {
+    console.log("Obtuviste", calificacion,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", calificacion,". No aprobaste.");
+}
+let califrandom = Math.floor(Math.random() * 101)
+console.log("Este segundo ejemplo muestra una calificación con una variable que asigna un número entre el 0 y 100 aleatoriamente. Actualiza tu navegador y el número y resultado también se actualizará.")
+if (califrandom >=90) {
+    console.log("Obtuviste", califrandom, ". ¡Aprobaste con una calificación excelente!");
+} else if (califrandom >=75 && califrandom <=89) {
+    console.log("Obtuviste", califrandom,". ¡Aprobaste con una calificación buena!");
+} else if (califrandom >=60 && califrandom <=74) {
+    console.log("Obtuviste", califrandom,". Aprobaste con una calificación suficiente.");
+} else {
+    console.log("Obtuviste", califrandom,". No aprobaste.");
+}
